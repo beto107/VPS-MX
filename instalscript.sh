3f877a000ac39833bc6a20769fd9c8899df5a8b8
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-05-02 13:06:30 -0400

    Update Script

--- instalscript.sh
+++ instalscript.sh
@@ -52,7 +52,7 @@ msg -bar
 	echo -e "\033[1;97m           ---- QUE UBUNTU ESTA USANDO ----"
 	echo -e "\033[1;97m  Digite solo el numero segun su respuesta: "
     msg -bar
-    echo -e "\033[1;97m Escoja la opcion deseada. (Default Opcion 4)"
+    echo -e "\033[1;97m Escoja la opcion deseada. \033[1;93m(Default Opcion 4)\033[0m"
     msg -bar
     echo " 1).- Ubuntu 16.04 "
     echo " 2).- Ubuntu 18.04 "
